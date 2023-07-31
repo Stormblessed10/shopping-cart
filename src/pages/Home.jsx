62edd15d4f3b4eaeb4e793b7b42575d78331a69e
Stormblessed <carlomanid@gmail.com>
2023-08-01 00:35:40 +0300

    Minor fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,20 @@ import { Link } from "react-router-dom";
 export default function Home() {
     return <div className={styles.home}>
         <div className={styles.info}>
-            <div>
-                <h1>Game Store</h1>
-                <p>Application imitating a game store made with React and React Router with use of Rawg API</p>
+            <div className={`${styles.wrap}`}>
+                <h1>Welcome to Shadowed Sanctum</h1>
+                <p>This application is designed to emulate a game store where you can browse and explore a vast collection of games. You can't purchase games from this platform. <br/>
+               </p>
             </div>
-            <div className={styles.links}>
-                <a href="https://github.com/Stormblessed10/shopping-cart"><img src="/icons/github.svg" alt="star"/> GitHub</a>
-                <a href="https://rawg.io/">RAWG API</a>
+            <div className={`${styles.wrap}`}>
+                <p>App is built using React and React Router, and integrated the Rawg API to ensure that you have access to the latest and greatest games.</p>
+                <div className={styles.links}>
+                    <a href="https://github.com/Stormblessed10/shopping-cart"><img src="/icons/github.svg" alt="star"/> GitHub</a>
+                    <a href="https://rawg.io/">RAWG API</a>
+                </div>
             </div>
         </div>
-        <nav className={styles.nav}>
+        <nav className={`${styles.nav} ${styles.wrap}`}>
             <ul>
                 <li><h2>Quick Navigation</h2></li>
                 <li><Link to="shop"><img src="/icons/star.svg" alt="star"/> Last 30 days</Link></li>
