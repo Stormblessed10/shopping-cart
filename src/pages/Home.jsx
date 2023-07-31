9f65a3a399bcff2c4db61c140cae20269d21677d
Stormblessed <carlomanid@gmail.com>
2023-07-31 21:20:34 +0300

    Add icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,19 +6,20 @@ export default function Home() {
         <div className={styles.info}>
             <div>
                 <h1>Game Store</h1>
-                <p>Application imitating a game store made with React, Redux and React Router with use of  Rawg API</p>
+                <p>Application imitating a game store made with React and React Router with use of Rawg API</p>
             </div>
             <div className={styles.links}>
-                <a href="https://github.com/Stormblessed10/shopping-cart">GitHub</a>
+                <a href="https://github.com/Stormblessed10/shopping-cart"><img src="/icons/github.svg" alt="star"/> GitHub</a>
                 <a href="https://rawg.io/">RAWG API</a>
             </div>
         </div>
         <nav className={styles.nav}>
             <ul>
                 <li><h2>Quick Navigation</h2></li>
-                <li><Link to="shop">All time top</Link></li>
-                <li><Link to="shop">PC</Link></li>
-                <li><Link to="shop">RPG</Link></li>
+                <li><Link to="shop"><img src="/icons/star.svg" alt="star"/> Last 30 days</Link></li>
+                <li><Link to="shop"><img src="/icons/top.svg" alt="star"/> All time top</Link></li>
+                <li><Link to="shop"><img src="/icons/playstation.svg" alt="star"/> PlayStation</Link></li>
+                <li><Link to="shop"><img src="/icons/rpg.svg" alt="star"/> RPG</Link></li>
             </ul>
         </nav>
     </div>
