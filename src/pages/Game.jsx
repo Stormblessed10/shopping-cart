0afad3a6918e8bf8b2149166846109a9a219803b
Stormblessed <carlomanid@gmail.com>
2023-08-01 23:44:28 +0300

    Minor fix

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,29 +1,44 @@
 import styles from "./Game.module.css";
 import Carousel from "../components/Carousel";
 import { Link, useLoaderData, useLocation } from "react-router-dom";
+import { useGames } from "../context/GamesContext";
+import { useState } from "react";
 
 export default function Game() {
+    const [alreadyAdd, setAlreadyAdd] = useState(false);
     const {game, screenshots} = useLoaderData();
     const {state} = useLocation();
+    const { setCart } =  useGames();
 
     const price = +game.rating ? Math.round(game.rating) * 5 - 0.01 : 4.99;
     const backgroundImage = {
         id: "background",
         image: game.background_image,
     }
+    
+    function handleAddToCart() {
+        setCart(cart => [...cart, {name: game.name, img: game.background_image, price, id: game.id}]);
+        setAlreadyAdd(true);
+    }
+
+    function handleRemoveFromCart() {
+        setCart(cart => cart.filter(({ id }) => id !== game.id));
+        setAlreadyAdd(false);
+    }
 
     return <section className={styles.game}>
-        <div className={styles.wrapper}>
+        <header className={styles.wrapper}>
             <Link to={state?.previousPath || "/"} className={styles.back}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z"></path>
                 </svg>
             </Link>
             <h1>{game.name}</h1>
-        </div>
+        </header>
         <div className={styles.wrapper}>
             <div>
-            <button className={styles.buy}>Add to cart +</button>
+                {!alreadyAdd && <button onClick={handleAddToCart} className={styles.buy}>+ Add to cart</button>}
+                {alreadyAdd && <button onClick={handleRemoveFromCart} className={styles.buy}>- Remove from cart</button>}
                 <ul className={styles.details}>
                     <li>
                         <h2>Price</h2>
@@ -71,6 +86,9 @@ const KEY = import.meta.env.VITE_KEY;
 
 export async function loader({ params }) {
     const res = await Promise.allSettled([fetch(`${URL}/${params.id}?key=${KEY}`), fetch(`${URL}/${params.id}/screenshots?key=${KEY}`)]);
+    // const heh = await fetch(`https://api.rawg.io/api/stores?key=${KEY}`);
+    // const heehe = await heh.json();
+    // console.log(heehe)
     const data1 = await res[0].value.json();
     const data2 = await res[1].value.json();
     return {game: data1, screenshots: data2};
