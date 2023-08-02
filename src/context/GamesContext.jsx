0f37b26c340772a811e358ea015c5933627e0e60
Stormblessed <carlomanid@gmail.com>
2023-08-02 17:08:33 +0300

    Add context for cart

--- src/context/GamesContext.jsx
+++ src/context/GamesContext.jsx
@@ -1,8 +1,15 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const GamesContext = createContext();
 
 export function GamesProvider({ children }) {
+    const [cart, setCart] = useState([]);
+    const [isCartOpen, setIsCartOpen] = useState(false);
 
-    return <GamesContext.Provider>{children}</GamesContext.Provider>
+    return <GamesContext.Provider value={{ cart, setCart, isCartOpen, setIsCartOpen }}>{children}</GamesContext.Provider>
+}
+
+export function useGames() {
+   const context = useContext(GamesContext);
+   return context;
 }
