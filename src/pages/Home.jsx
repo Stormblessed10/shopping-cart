c523507919fc8a0e49170fed63244529b1ad6ea2
Stormblessed <carlomanid@gmail.com>
2023-08-03 00:21:56 +0300

    Change links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import styles from "./Home.module.css";
 import { Link } from "react-router-dom";
+import { 
+    SvgTop,
+    SvgStar,
+    SvgBest,
+    SvgNextWeek,
+    SvgThisWeek 
+} from "../components/Svgs.jsx";
 
 export default function Home() {
-    return <div className={styles.home}>
+    return <section className={styles.home}>
         <div className={styles.info}>
             <div className={`${styles.wrap}`}>
                 <h1>Welcome to Shadowed Sanctum</h1>
-                <p>This application is designed to emulate a game store where you can browse and explore a vast collection of games. You can't purchase games from this platform. <br/>
+                <p>This application is designed to emulate a game store where you can browse and explore a vast collection of games. You can&apos;t purchase games from this platform. <br/>
                </p>
             </div>
             <div className={`${styles.wrap}`}>
@@ -20,11 +27,13 @@ export default function Home() {
         <nav className={`${styles.nav} ${styles.wrap}`}>
             <ul>
                 <li><h2>Quick Navigation</h2></li>
-                <li><Link to="shop"><img src="/icons/star.svg" alt="star"/> Last 30 days</Link></li>
-                <li><Link to="shop"><img src="/icons/top.svg" alt="star"/> All time top</Link></li>
-                <li><Link to="shop"><img src="/icons/playstation.svg" alt="star"/> PlayStation</Link></li>
-                <li><Link to="shop"><img src="/icons/rpg.svg" alt="star"/> RPG</Link></li>
+                <li><Link to="/shop/releases/all-time-top/1"><span><SvgTop/></span>All time top</Link></li>
+                <li><Link to="/shop/top/best-of-the-year/1"><span><SvgBest/></span>Best of the year</Link></li>
+                <li><Link to="/shop/releases/last-30-days/1"><span><SvgStar/></span>Last 30 days</Link></li>
+                <li><Link to="/shop/releases/next-week/1"><span><SvgNextWeek/></span>Next week</Link></li>
+                <li><Link to="/shop/releases/this-week/1"><span><SvgThisWeek/></span>This week</Link></li>
             </ul>
         </nav>
-    </div>
+        <video className={styles.video} autoPlay muted loop src="/bg.mp4"></video>
+    </section>
 }
