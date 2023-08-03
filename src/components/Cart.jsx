4543db8ade6a5b3de5257e257299bf77013a098e
Stormblessed <carlomanid@gmail.com>
2023-08-03 18:09:13 +0300

    implement scroll hiding

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,25 @@
+import { useEffect } from "react";
 import { useGames } from "../context/GamesContext";
 import styles from "./Cart.module.css";
 import CartItem from "./CartItem";
 
 export default function Cart() {
-    const { cart, setIsCartOpen } = useGames();
+    const { cart, setIsCartOpen, setCart } = useGames();
+
+    useEffect(() => {
+        document.body.classList.add("hide-scroll");
+
+        return function() {
+            document.body.classList.remove("hide-scroll");
+        }
+    }, [])
 
     return <>
         <div onClick={() => setIsCartOpen(false)} className={styles.overlay}></div>
         <div className={styles.cart}>
             <header>
                 <h2>{cart.length} Games</h2>
-                <button>Clear</button>
+                <button onClick={() => setCart([])}>Clear</button>
             </header>
 
             {cart.map(game => <CartItem key={game.id} game={game}/>)}
