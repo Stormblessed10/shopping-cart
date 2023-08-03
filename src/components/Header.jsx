72c4334f648444b283b060c7bf242854290bcd3f
Stormblessed <carlomanid@gmail.com>
2023-08-03 18:04:01 +0300

    Separate search in new component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { useGames } from "../context/GamesContext";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 import Cart from "./Cart";
+import Search from "./Search";
 
 export default function Header() {
     const { cart, setIsCartOpen, isCartOpen } =  useGames();
@@ -9,10 +10,7 @@ export default function Header() {
     return <>
     <header className={styles.header}>
         <Link className={styles.logo} to="">Shadowed Sanctum</Link>
-        <div className={styles.search}>
-            <input type="text" placeholder="Search..."/>
-            <span><img src="/icons/search.svg" alt="search" /></span>
-        </div>
+        <Search/>
         <button onClick={() => setIsCartOpen(true)} className={styles.cart}><img src="/icons/cart.svg" alt="cart" />
             {cart[0] && <span></span>}
         </button>
---
+++ src/components/Search.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "./Search.module.css";
+import { Link } from "react-router-dom";
+
+const URL = "https://api.rawg.io/api/games";
+const KEY = import.meta.env.VITE_KEY;
+
+export default function Search() {
+    const [input, setInput] = useState("");
+    const [focus, setFocus] = useState(false);
+    const [games, setGames] = useState([]);
+    const inputEl = useRef(null);
+
+    function handleHide() {
+        inputEl.current.blur();
+    }
+
+    useEffect(() => {
+        const controller = new AbortController();
+        async function getGames() {
+            try {
+                const res = await fetch(`${URL}?key=${KEY}&search=${input}&page_size=10`, {signal: controller.signal});
+                const data = await res.json();
+                setGames(data.results);
+            } catch(err) {
+                console.log(err)
+            }
+        }
+
+        if (input.length >= 3) getGames();
+
+        return function() {
+            controller.abort();
+        }
+    }, [input])
+
+    return <div className={styles.search}>
+        <div className={styles["search-bar"]}>
+            <input ref={inputEl} onBlur={() => setFocus(false)} onFocus={() => setFocus(true)} value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder="Search..."/>
+            <span><img src="/icons/search.svg" alt="search" /></span>
+        </div>
+        <ul onMouseDown={(e) => e.preventDefault()} className={`${styles.results} ${focus && games[0] && styles.visible}`}>
+            {games.map(game => <li onClick={handleHide} key={game.id} className={styles["cart-item"]}>
+            <Link to={"/game/" + game.id}>
+                <img src={game.background_image} alt={game.name}/>
+                <p>{game.name}</p>
+            </Link>
+            </li>)}
+        </ul>
+    </div>
+}
