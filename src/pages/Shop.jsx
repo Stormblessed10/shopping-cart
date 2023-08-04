8c5998544fb19cf31c961418b0ef7f853ac38b86
Stormblessed <carlomanid@gmail.com>
2023-08-04 16:43:25 +0300

    Add error

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,21 +1,60 @@
+import { useLoaderData } from "react-router-dom";
 import Aside from "../components/Aside";
 import Content from "../components/Content";
+import PageNotFound from "./PageNotFound";
+
 
 export default function Shop() {
+    const games = useLoaderData();
+
+    if (games === null) return <PageNotFound/>
 
     return <>
         <Aside/>
-        <Content/>
+        <Content games={games}/>
     </>
+}
+
+function dateAgo(days) {
+    return new Date(new Date().setDate(new Date().getDate() - days)).toISOString().split("T")[0];
 }
 
 const URL = "https://api.rawg.io/api/games";
 const KEY = import.meta.env.VITE_KEY;
 
 export async function loader({ params }) {
-    const {id, category} = params;
-    const query = `${URL}?key=${KEY}&${id ? category + "=" + id : ""}&page_size=21`;
-    const res = await fetch(query);
-    const data = await res.json();
-    return data;
+    const {id, category, page} = params;
+
+    try {
+        let query = "";
+        const today = new Date().toISOString().split("T")[0];
+    
+        switch(category+"/"+id) {
+            case "releases/last-30-days":
+                query = `${URL}?key=${KEY}&dates=${dateAgo(30)},${today}&page_size=21&page=${page}`;
+                break;
+            case "releases/this-week":
+                query = `${URL}?key=${KEY}&dates=${dateAgo(7)},${today}&page_size=21&page=${page}`;
+                break;
+            case "releases/next-week":
+                query = `${URL}?key=${KEY}&dates=${today},${dateAgo(-7)}&page_size=21&page=${page}`;
+                break;
+            case "top/all-time-top":
+                query = `${URL}?key=${KEY}&ordering=-metacritic&page_size=21&page=${page}`;
+                break;
+            case "top/best-of-the-year":
+                query = `${URL}?key=${KEY}&ordering=-metacritic&dates=${dateAgo(365)},${dateAgo(0)}&page_size=21&page=${page}`;
+        }
+    
+        if (["platforms", "genres"].includes(category)) {
+            query = `${URL}?key=${KEY}&${category + "=" + id}&page_size=21&page=${page}`;
+        }
+    
+        const res = await fetch(query);
+        const data = await res.json();
+        if (!data?.count) return null;
+        return data;
+    } catch {
+        return null;
+    }
 }
