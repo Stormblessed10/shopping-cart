fa215dfc8d4e92044fdc69eb052fe74da0d4f2f8
Stormblessed <carlomanid@gmail.com>
2023-08-04 16:44:28 +0300

    Add details

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,12 +1,15 @@
 import styles from "./Game.module.css";
 import Carousel from "../components/Carousel";
-import Cart from "../components/Cart";
-import AddButton from "../components/AddButton";
+import AddToCart from "../components/AddToCart";
 import { Link, useLoaderData, useLocation } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import GameDetails from "../components/GameDetails";
 
 export default function Game() {
     const { game, screenshots } = useLoaderData();
     const { state } = useLocation();
+
+    if (game === null) return <PageNotFound/>;
 
     const price = +game.rating ? Math.round(game.rating) * 5 - 0.01 : 4.99;
     const backgroundImage = {
@@ -14,8 +17,8 @@ export default function Game() {
         image: game.background_image,
     }
 
-    return <section className={styles.game}>
-        <Cart></Cart>
+
+    return <section key={game.id} className={styles.game}>
         <header className={styles.wrapper}>
             <Link to={state?.previousPath || "/"} className={styles.back}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -26,38 +29,8 @@ export default function Game() {
         </header>
         <div className={styles.wrapper}>
             <div>
-               <AddButton game={game} fz={"2.5rem"} price={price}/>
-                <ul className={styles.details}>
-                    <li>
-                        <h2>Price</h2>
-                        <div>${price}</div>
-                    </li>
-                    <li>
-                        <h2>Released</h2>
-                        <div>{game.released.replaceAll("-", "/")}</div>
-                    </li>
-                    <li>
-                        <h2>Genres</h2>
-                        <div>{game.genres.map(({name}) => <span key={name}>{name}</span>)}</div>
-                    </li>
-                    <li>
-                        <h2>Platforms</h2>
-                        <div>{game.platforms.map(({platform}) => <span key={platform.name}>{platform.name}</span>)}</div>
-                    </li>
-                    <li>
-                        <h2>Developers</h2>
-                        <div>{game.developers.map(({name}) => <span key={name}>{name}</span>)}</div>
-                    </li>
-                    <li>
-                        <h2>Publishers</h2>
-                        <div>{game.publishers.map(({name}) => <span key={name}>{name}</span>)}</div>
-                    </li>
-                    <li>
-                        <h2>Metascore</h2>
-                        {!game.metacritic && "-"}
-                        {game.metacritic && <div className={styles["metacritic-" + (game.metacritic >= 75 ? "green" : game.metacritic >= 50 ? "yellow" : "red")]}>{game.metacritic}</div>}
-                    </li>
-                </ul>
+               <AddToCart style={{ fontSize: "2.5rem", paddingLeft: "3rem" }} game={{...game, img: game.background_image, price}} />
+                <GameDetails game={game} styles={styles} price={price}/>
             </div>
             <Carousel images={[backgroundImage, ...screenshots.results]}></Carousel>
         </div> 
@@ -73,11 +46,15 @@ const URL = "https://api.rawg.io/api/games";
 const KEY = import.meta.env.VITE_KEY;
 
 export async function loader({ params }) {
-    const res = await Promise.allSettled([fetch(`${URL}/${params.id}?key=${KEY}`), fetch(`${URL}/${params.id}/screenshots?key=${KEY}`)]);
-    // const heh = await fetch(`https://api.rawg.io/api/stores?key=${KEY}`);
-    // const heehe = await heh.json();
-    // console.log(heehe)
-    const data1 = await res[0].value.json();
-    const data2 = await res[1].value.json();
-    return {game: data1, screenshots: data2};
+    try {
+        const res = await Promise.allSettled([fetch(`${URL}/${params.id}?key=${KEY}`), fetch(`${URL}/${params.id}/screenshots?key=${KEY}`)]);
+        const data1 = await res[0].value.json();
+        const data2 = await res[1].value.json();
+        
+        if (!data1.name) throw new Error();
+
+        return {game: data1, screenshots: data2};
+    } catch {
+        return {game: null, screenshots: null};
+    }
 }
