57f5cccfbc3ab7f14d9a4cb43392a6bd9fc4075b
Stormblessed <carlomanid@gmail.com>
2023-08-05 15:23:51 +0300

    Add lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import Shop, { loader as shopLoader } from "./pages/Shop";
+import { loader as shopLoader } from "./pages/Shop";
 import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import AppLayout from './pages/AppLayout';
-import Home from './pages/Home';
 import { GamesProvider } from "./context/GamesContext";
-import Game, { loader as gameLoader } from "./pages/Game";
-import PageNotFound from "./pages/PageNotFound";
+import { loader as gameLoader } from "./pages/Game";
+import { Suspense, lazy } from "react";
+import Spinner from "./components/Spinner";
+
+const Home = lazy(() =>import("./pages/Home"));
+const Shop = lazy(() =>import("./pages/Shop"));
+const Game = lazy(() =>import("./pages/Game"));
+const PageNotFound = lazy(() =>import("./pages/PageNotFound"));
 
 const router = createBrowserRouter([
   {
@@ -46,5 +51,9 @@ const router = createBrowserRouter([
 ]);
 
 export default function App() {
-  return <GamesProvider><RouterProvider router={router}/></GamesProvider>
+  return <GamesProvider>
+      <Suspense fallback={<Spinner/>}>
+        <RouterProvider router={router}/>
+      </Suspense>
+    </GamesProvider>
 }
