c2a1ec74f8c108ddd85669762d7ecac8d8b6ddfa
Stormblessed <carlomanid@gmail.com>
2023-08-12 18:06:32 +0300

    Add state

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,16 +2,25 @@ import { useLoaderData } from "react-router-dom";
 import Aside from "../components/Aside";
 import Content from "../components/Content";
 import PageNotFound from "./PageNotFound";
+import styles from "./Shop.module.css";
+import { useState } from "react";
 
 
 export default function Shop() {
+    const [isClosed, setIsClosed] = useState(false);
     const games = useLoaderData();
 
     if (games === null) return <PageNotFound/>
 
     return <>
-        <Aside/>
+        <div className={`${!isClosed ? styles.closed : styles.opened}`}></div>
+        <Aside className={!isClosed ? styles.hidden : styles.visible}/>
         <Content games={games}/>
+        <button onClick={() => setIsClosed(!isClosed)} className={`${styles.btn}`}>
+            {isClosed ? <svg fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"></path></svg> :
+            <svg fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"></path></svg>
+            }
+        </button>
     </>
 }
 
