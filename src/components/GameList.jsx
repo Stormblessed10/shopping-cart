1334110b716a49a820c3d2b3bec671584ef0b7c1
Stormblessed <carlomanid@gmail.com>
2023-08-13 17:40:50 +0300

    Dist commit

--- src/components/GameList.jsx
+++
@@ -1,9 +0,0 @@
-import GameListItem from "./GameListItem";
-import styles from "./GameList.module.css";
-
-export default function GameList({ games }) {
-
-    return <ul className={styles.list}>
-        {games.results.map(game => <GameListItem key={game.id} game={game}/>)}
-    </ul>
-}
